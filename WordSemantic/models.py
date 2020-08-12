698d1446e0d42f83ebcb70c1e4b01db499df9c8f
iadnanmalik <61354025+iadnanmalik@users.noreply.github.com>
2020-08-13 00:10:29 +0500

    Developed Homepage UI

--- WordSemantic/models.py
+++ WordSemantic/models.py
@@ -9,7 +9,6 @@ class Label(models.Model):
 class WordSemantic(models.Model):
     sentence =models.CharField(max_length=100000)
     flag= models.BooleanField(default=0)
-    labels = ArrayField(models.CharField(max_length=50), blank=True)
     username= models.CharField(max_length=25)
     def __str__(self):
         return self.sentence
